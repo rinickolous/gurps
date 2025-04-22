53a683fa1a69c847ec0dd7cba599749bbca5963e
mjeffw <geek.macbeer@gmail.com>
2025-04-22 15:31:05 -0700

    Refactor token selection logic in DamageChat to ensure compatibility with GURPS Token Shapes and Movement

--- module/damage/damagechat.js
+++ module/damage/damagechat.js
@@ -447,8 +447,24 @@ export default class DamageChat {
       return false
     }
 
-    // Create a "reactangle" to represent the drop coordinates. It is really a point as width and height are 0.
-    let selectedTokens = await DamageChat.selectTokensAtPosition(dropData.x, dropData.y)
+    // // Create a "reactangle" to represent the drop coordinates. It is really a point as width and height are 0.
+    // const rect = new PIXI.Rectangle(dropData.x, dropData.y, 0, 0)
+
+    // // Get all tokens under the point.
+    // let selectedTokens = canvas.tokens.quadtree
+    //   .getObjects(rect, {
+    //     collisionTest: o => o.t.hitArea.contains(dropData.x - o.t.x, dropData.y - o.t.y),
+    //   })
+    //   .values()
+    //   .toArray()
+
+    // Get all tokens under the drop point. Use this instead of the quadtree approach because
+    // GURPS Token Shapes and Movement manipulates the position of the tokens.
+    let selectedTokens = canvas.tokens.objects.children.filter(t =>
+      t.hitArea.contains(dropData.x - t.x, dropData.y - t.y)
+    )
+
+    if (selectedTokens.length > 1) selectedTokens = await selectTarget(selectedTokens)
 
     if (selectedTokens.length === 0) {
       selectedTokens = user?.targets.values().toArray()
